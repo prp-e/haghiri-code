45cf619c158f3ff3662328aff5cdc2e7290da175
Muhammadreza Haghiri <haghiri75@gmail.com>
2026-02-09 12:53:39 +0330

    working on it

--- src/main.py
+++ src/main.py
@@ -5,4 +5,16 @@ from openai import OpenAI
 load_dotenv()
 
 class Agent:
-    pass
+    def __init__(self, system_prompt):
+
+        self.system_prompt = system_prompt
+        self.messages = []
+
+        if self.system_prompt:
+            self.messages.append(
+                {
+                    "role" : "system",
+                    "content" : system_prompt
+                }
+            )
+    
